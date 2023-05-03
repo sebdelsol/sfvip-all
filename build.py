b2c10e0e8f015ac8bbf074a2a94388d136feadc0
sebdelsol <seb.morin@gmail.com>
2023-05-03 15:48:16 +0200

    zip the build
    Former-commit-id: a75b571262f008c3859f3accd7148f5d35bfa2c8

--- build.py
+++ build.py
@@ -1,16 +1,21 @@
+import shutil
+
 import PyInstaller.__main__
 
 from sfvip import Player
 from sfvip.config import CONFIG
 
+NAME = "sfvip_all"
 sfvip_player = Player(CONFIG)
 if sfvip_player.valid():
     PyInstaller.__main__.run(
         [
-            "sfvip_all.py",
+            f"{NAME}.py",
             "--noconfirm",
             "--noconsole",
             "-i",
             rf"{sfvip_player.player},0",
         ]
     )
+    print("create the zip archive")
+    shutil.make_archive(NAME, "zip", f"./dist/{NAME}")
