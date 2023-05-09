a8dac78962307a0dd3f441f75867c2287d488e73
sebdelsol <seb.morin@gmail.com>
2023-05-09 02:06:47 +0200

    build.dist_temp

--- build.py
+++ build.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 import shutil
 import subprocess
 import sys
@@ -9,19 +10,19 @@ from build_config import Build, Github
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--readme-only", action="store_true", help="update readme and post only")
-update_distribution = not parser.parse_args().readme_only
+update_dist = not parser.parse_args().readme_only
 
-installer_dir = f"{Build.dir}/installer"
-built_name = f"{Build.dir}/{Build.version}/{Build.name}"
+dist_temp = f"{Build.dir}/temp"
+dist_name = f"{Build.dir}/{Build.version}/{Build.name}"
 
-if update_distribution:
+if update_dist:
     subprocess.run(
         [
             *(sys.executable, "-m", "nuitka"),  # run nuitka
             f"--windows-file-version={Build.version}",
             f"--windows-icon-from-ico={Build.ico}",
             f"--output-filename={Build.name}.exe",
-            f"--output-dir={installer_dir}",
+            f"--output-dir={dist_temp}",
             "--assume-yes-for-downloads",  # download dependency walker, ccache, and gcc
             "--enable-plugin=tk-inter",  # needed for tkinter
             "--prefer-source-code",
@@ -35,15 +36,15 @@ if update_distribution:
         check=True,
     )
     print(f"Compress {Build.name}.zip")
-    shutil.copy(f"{installer_dir}/{Build.name}.exe", f"{built_name}.exe")
-    shutil.make_archive(built_name, "zip", f"{installer_dir}/{Build.main[:-3]}.dist")
+    shutil.make_archive(dist_name, "zip", f"{dist_temp}/{os.path.splitext(Build.main)[0]}.dist")
+    shutil.copy(f"{dist_temp}/{Build.name}.exe", f"{dist_name}.exe")
 
 # Create readme.md & forum post
 print("Create readme & forum post")
 template_format = dict(
     github_path=f"{Github.owner}/{Github.repo}",
-    archive_link=quote(f"{built_name}.zip"),
-    exe_link=quote(f"{built_name}.exe"),
+    archive_link=quote(f"{dist_name}.zip"),
+    exe_link=quote(f"{dist_name}.exe"),
     ico_link=quote(Build.ico),
     version=Build.version,
     name=Build.name,
