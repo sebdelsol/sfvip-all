cf0f304012695fd05a228506f99786de03010355
sebdelsol <seb.morin@gmail.com>
2023-05-16 01:56:23 +0200

    debloat mitmdump
    Former-commit-id: b751e2114cfe33dfcfe35263598564808c4c4a05

--- proxy/__init__.py
+++ proxy/__init__.py
@@ -5,11 +5,32 @@ import re
 import threading
 from typing import Any, Optional
 
-from mitmproxy import addons, http, options
+from mitmproxy import http, options
+from mitmproxy.addons import (
+    core,
+    disable_h2c,
+    dns_resolver,
+    next_layer,
+    proxyserver,
+    tlsconfig,
+)
 from mitmproxy.coretypes.multidict import MultiDictView
 from mitmproxy.master import Master
 
 from sfvip_all_config import AllCat
+
+
+# warning: use only the needed addons,
+# if any issues use addons.default_addons() instead
+def _minimum_addons():
+    return [
+        core.Core(),
+        disable_h2c.DisableH2C(),
+        proxyserver.Proxyserver(),
+        dns_resolver.DnsResolver(),
+        next_layer.NextLayer(),
+        tlsconfig.TlsConfig(),
+    ]
 
 
 class _AddOn:
@@ -41,17 +62,17 @@ class _AddOn:
     def _remove_query_key(self, request: http.Request, key: str) -> None:
         del self._query(request)[key]
 
-    def _api_query(self, request: http.Request, key: str) -> Optional[str]:
+    def _get_query_key(self, request: http.Request, key: str) -> Optional[str]:
         return self._query(request).get(key, None) if self._is_api_request(request) else None
 
     def request(self, flow: http.HTTPFlow) -> None:
-        category_id = self._api_query(flow.request, "category_id")
+        category_id = self._get_query_key(flow.request, "category_id")
         if category_id is not None and category_id == self._all_cat_id:
             # turn an all category query into a whole catalog query
             self._remove_query_key(flow.request, "category_id")
 
     def response(self, flow: http.HTTPFlow) -> None:
-        action = self._api_query(flow.request, "action")
+        action = self._get_query_key(flow.request, "action")
         if action is not None and self._is_action_get_categories(action):
             if categories := self._response_json(flow.response):
                 if isinstance(categories, list):
@@ -81,7 +102,7 @@ class Proxy(multiprocessing.Process):
         mode = f"upstream:{self._upstream}" if self._upstream else "regular"
         opts = options.Options(listen_port=self._port, ssl_insecure=True, mode=(mode,))
         self._master = master = Master(opts, event_loop=loop)
-        master.addons.add(*addons.default_addons())
+        master.addons.add(*_minimum_addons())
         master.addons.add(_AddOn(self._all_cat))
         threading.Thread(target=self._wait_for_stop).start()
         loop.run_until_complete(master.run())
