73224d18023c1e3a7083ff0cf05b23270a02aa39
sebdelsol <seb.morin@gmail.com>
2023-05-18 14:17:07 +0200

    fix systemwide init_lock

--- sfvip/__init__.py
+++ sfvip/__init__.py
@@ -7,19 +7,32 @@ from build_config import Build
 from .accounts import Accounts
 from .config_loader import ConfigLoader
 from .local_proxies import LocalProxies
+from .mutex import SystemWideMutex
 from .player import Player
 from .ui import UI
 
+# TODO rename standalone exe results in multiprocessing unable to launch sfvip all.exe
+
 
 def run():
-    config_loader = ConfigLoader(Config, Path(os.getenv("APPDATA")) / Build.name / "Config.json")
+    config_file = Path(os.getenv("APPDATA")) / Build.name / "Config.json"
+    config_loader = ConfigLoader(Config, config_file)
     config_loader.update()
+
     ui = UI(Build.name, Build.splash)
     player = Player(Config.Player, config_loader, ui)
     ui.splash.show(player.rect)
-    accounts = Accounts(Build.name, player.config_dir)
+
+    init_lock = SystemWideMutex(Build.name, acquire=True)
+    accounts = Accounts(player.config_dir)
     with LocalProxies(Config.AllCat, accounts.upstream_proxies) as local_proxies:
-        with accounts.set_proxies(local_proxies.by_upstreams) as restore_accounts_proxies:
+        restore = accounts.set_proxies(local_proxies.by_upstreams)
+        try:
             with player.run():
-                restore_accounts_proxies()
+                accounts.wait_being_read()
+                accounts.set_proxies(restore)
+                init_lock.release()
                 ui.splash.hide()
+        finally:
+            with init_lock:
+                accounts.set_proxies(restore)
