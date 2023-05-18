abdcf28e790049120d6426d6b6a63337be09f6e2
sebdelsol <seb.morin@gmail.com>
2023-05-18 21:07:30 +0200

    tk mainloop
    Former-commit-id: 595478a637745a8e2a6f8bc8b026c94873c2564e

--- sfvip/ui.py
+++ sfvip/ui.py
@@ -1,9 +1,9 @@
-import time
+import threading
 import tkinter as tk
 from collections import namedtuple
 from pathlib import Path
 from tkinter import filedialog, messagebox
-from typing import Self
+from typing import Callable, Self
 
 
 class Rect(namedtuple("rect", "x y w h", defaults=[None] * 4)):
@@ -30,54 +30,77 @@ class Rect(namedtuple("rect", "x y w h", defaults=[None] * 4)):
         return f"{self.w}x{self.h}+{self.x:.0f}+{self.y:.0f}"
 
 
-class UI:
-    """tk without a mainloop"""
+class UI(tk.Tk):
+    """basic UI with a tk mainloop, the app has to run in a thread"""
 
-    class Splash:
-        _fade_dt = 0.025
-        _bg = "black"
+    def __init__(self, app_name: str, splash_path) -> None:
+        super().__init__()
+        self.withdraw()
+        file = Path(__file__).parent.parent / splash_path
+        image = self._image = tk.PhotoImage(file=file)
+        self._image = image  # keep a reference for tk
+        self.wm_iconphoto(True, image)
+        self.app_name = app_name
 
-        def __init__(self, root: tk.Tk, splash_path: str) -> None:
-            file = Path(__file__).parent.parent / splash_path
-            self._image = tk.PhotoImage(file=file)  # keep a reference for tk
-            self._root = root
+        class Splash:
+            """use tk root as a splash screen"""
 
-        def show(self, rect: Rect) -> None:
-            """show in the middle of rect"""
-            if rect.valid():
-                screen = Rect(0, 0, self._root.winfo_screenwidth(), self._root.winfo_screenheight())
-                geom = rect.keep_in(screen).center_for(self._image.width(), self._image.height()).to_geometry()
-                self._root.geometry(geom)
-            else:
-                self._root.eval("tk::PlaceWindow . Center")
-            tk.Label(self._root, bg=UI.Splash._bg, image=self._image).pack()
-            self._root.attributes("-transparentcolor", UI.Splash._bg, "-topmost", True, "-alpha", 1.0)
-            self._root.overrideredirect(True)
-            self._root.deiconify()
-            self._root.update()
+            _fade_dt_ms: int = 25
+            _bg = "black"
 
-        def hide(self, fade_duration: float = 0.5) -> None:
-            """BLOCKING fade out for fade_duration, use when everything else is done"""
-            if fade_duration > 0:
-                dalpha = UI.Splash._fade_dt / fade_duration
-                while (alpha := self._root.attributes("-alpha") - dalpha) > 0:
-                    self._root.attributes("-alpha", alpha)
-                    time.sleep(UI.Splash._fade_dt)
-            self._root.withdraw()
+            @staticmethod
+            def show(rect: Rect) -> None:
+                """show in the middle of rect"""
+                if rect.valid():
+                    screen = Rect(0, 0, self.winfo_screenwidth(), self.winfo_screenheight())
+                    geom = rect.keep_in(screen).center_for(image.width(), image.height()).to_geometry()
+                    self.geometry(geom)
+                else:
+                    self.eval("tk::PlaceWindow . Center")
+                tk.Label(self, bg=Splash._bg, image=image).pack()
+                self.attributes("-transparentcolor", Splash._bg, "-topmost", True, "-alpha", 1.0)
+                self.overrideredirect(True)
+                self.deiconify()
 
-    def __init__(self, title: str, splash_path) -> None:
-        root = tk.Tk()
-        root.withdraw()
-        self.splash = UI.Splash(root, splash_path)
-        root.wm_iconphoto(True, self.splash._image)
-        self._title = title
+            @staticmethod
+            def hide(fade_duration_ms: int = 500):
+                """fade out and withdraw"""
+                dalpha = Splash._fade_dt_ms / fade_duration_ms if fade_duration_ms > 0 else 1.0
+
+                def _hide() -> None:
+                    if (alpha := self.attributes("-alpha") - dalpha) > 0:
+                        self.attributes("-alpha", alpha)
+                        self.after(Splash._fade_dt_ms, _hide)
+                    else:
+                        self.withdraw()
+
+                _hide()
+
+        self.splash = Splash
+
+    def in_thread(self, target: Callable[..., None], *args) -> None:
+        """
+        run the target function in a thread,
+        handle the mainloop and quit ui when done
+        """
+
+        def _run():
+            try:
+                target(*args)
+            finally:
+                self.after(0, self.quit)
+
+        thread = threading.Thread(target=_run)
+        thread.start()
+        self.mainloop()
+        thread.join()
 
     def showinfo(self, message: str) -> None:
-        messagebox.showinfo(self._title, message=message)
+        messagebox.showinfo(self.app_name, message=message)
 
     def find_file(self, name: str, pattern: str) -> None:
-        title = f"{self._title}: Find {name}"
+        title = f"{self.app_name}: Find {name}"
         return filedialog.askopenfilename(title=title, filetypes=[(name, pattern)])
 
     def askretry(self, message: str) -> None:
-        return messagebox.askretrycancel(self._title, message=message)
+        return messagebox.askretrycancel(self.app_name, message=message)
