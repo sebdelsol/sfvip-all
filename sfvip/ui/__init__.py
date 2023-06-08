4ba438aacd759ec4be62b622c8cd1900ccc81114
sebdelsol <seb.morin@gmail.com>
2023-06-08 12:42:40 +0200

    flat scrollbar
    Former-commit-id: 813158e1adfbd8fc99466e4e8bb25adb47794283

--- sfvip/ui/__init__.py
+++ sfvip/ui/__init__.py
@@ -9,39 +9,33 @@ from winapi import set_click_through
 from .fx import _Fade, _Pulse
 from .sticky import Rect, WinState, _Offset, _StickyWindow
 from .style import _Style
-from .widget import _Button, _ListView, _VscrollCanvas
+from .widget import _Button, _ListView, _set_border, _set_vscrollbar_style
 
 
 class TH:
-    bg = "#2A2A2A"
+    bg = "#242424"
+    bg2 = "#2A2A2A"
+    separator = "#303030"
 
 
 class InfoTH:
     pad = 5
     bg = TH.bg
+    border = dict(bg="#808080", size=1)
+    button = dict(bg="#1F1E1D", mouseover="#3F3F41", bd_color="white", bd_size=0.75, bd_relief="groove")
 
     class Listview:
-        headers = TH.bg
-        rows = "#242424"
-        sep = "#303030"
-
-    class Button:
-        bg = "#1F1E1D"
-        mouseover = "#3F3F41"
-
-    class Border:
-        bg = "#808080"
-        bd = 1
+        colors = dict(bg_headers=TH.bg, bg_row=TH.bg2, bg_separator=TH.separator)
+        scrollbar = dict(bg=TH.bg2, slider="white", active_slider="grey")
 
 
 class StlInfo:
-    stl = _Style().font("Calibri").font_size(12)
+    stl = _Style().font("Calibri").font_size(12).max_width(40)
     arrow = stl("➞").color("#707070").bigger(5)
     upstream = stl.color("#A0A0A0")
     proxy = stl.white
-    blank = stl("")
     name = upstream
-    max_len = 30
+    blank = stl("")
 
 
 class Info(NamedTuple):
@@ -52,24 +46,21 @@ class Info(NamedTuple):
 
 def _get_infos_headers(app_name: str) -> tuple[_Style, ...]:
     return (
-        StlInfo.name("User Name").bigger(2).bold,  # type: ignore
+        StlInfo.name("User Name").bigger(2).bold,
         StlInfo.blank,
-        StlInfo.proxy(f"{app_name} Proxy").bigger(2).bold,  # type: ignore
+        StlInfo.proxy(f"{app_name} Proxy").bigger(2).bold,
         StlInfo.blank,
-        StlInfo.proxy("User Proxy").bigger(2).bold,
+        StlInfo.upstream("User Proxy").bigger(2).bold,
     )
 
 
-def get_row(info: Info) -> tuple[_Style, ...]:
-    name = info.name[: StlInfo.max_len]
-    proxy = info.proxy[: StlInfo.max_len]
-    upstream = info.upstream[: StlInfo.max_len]
+def _get_row(info: Info) -> tuple[_Style, ...]:
     return (
-        StlInfo.name(name),  # type: ignore
+        StlInfo.name(info.name),
         StlInfo.arrow,
-        StlInfo.proxy(proxy) if proxy else StlInfo.stl("No Proxy").red,
-        StlInfo.arrow if upstream else StlInfo.stl(""),
-        StlInfo.upstream(upstream) if upstream else StlInfo.stl("-").grey,
+        StlInfo.proxy(info.proxy) if info.proxy else StlInfo.stl("No Proxy").red,
+        StlInfo.arrow if info.upstream else StlInfo.blank,
+        StlInfo.upstream(info.upstream) if info.upstream else StlInfo.stl("-").grey,
     )
 
 
@@ -78,7 +69,7 @@ def _get_button_relaunch() -> _Style:
 
 
 def _get_version(app_name: str, app_version: str) -> _Style:
-    return StlInfo.stl(f"{app_name} {app_version}").smaller(2).grey
+    return StlInfo.stl(f"{app_name} v{app_version}").smaller(2).grey
 
 
 def is_valid(info: Info) -> bool:
@@ -96,57 +87,34 @@ class _InfosWindow(_StickyWindow):
     _max_height = 300
 
     def __init__(self, app_name: str, app_version: str) -> None:
-        super().__init__(
-            _InfosWindow._offset,
-            highlightbackground=InfoTH.Border.bg,
-            highlightthickness=InfoTH.Border.bd,
-            highlightcolor=InfoTH.Border.bg,
-            bg=InfoTH.bg,
-        )
+        border = _set_border(**InfoTH.border)  # type:ignore
+        super().__init__(_InfosWindow._offset, **border, bg=InfoTH.bg)
         self.attributes("-alpha", 0.0)
         self.maxsize(-1, _InfosWindow._max_height)
-        self._headers = _get_infos_headers(app_name)
-        self._fade = _Fade(self)
-        # create a frame for a working mouse hovering detection
+        # create a frame for hovering detection
         hover_frame = tk.Frame(self, bg=InfoTH.bg)
         hover_frame.pack(fill="both", expand=True)
         self._bind_mouse_hover(hover_frame)
         # create the widgets
-        self.create_widgets(hover_frame, app_name, app_version)
+        self._create_widgets(hover_frame, app_name, app_version)
+        self._headers = _get_infos_headers(app_name)
+        self._fade = _Fade(self)
 
-    def create_widgets(self, frame: tk.Frame, app_name: str, app_version: str) -> None:
-        # version
+    def _create_widgets(self, frame: tk.Frame, app_name: str, app_version: str) -> None:
         pad = InfoTH.pad
-        text = _get_version(app_name, app_version)
-        label = tk.Label(frame, bg=InfoTH.bg, **text.to_tk)
-        # relaunch button
-        text = _get_button_relaunch()
-        border = dict(bd_color="white", bd_width=0.75, bd_relief="groove")
-        buttonstyle = dict(bg=InfoTH.Button.bg, mouseover=InfoTH.Button.mouseover)
-        relaunch_button = _Button(frame, **buttonstyle, **border, **text.to_tk)
-        # separator
-        sep = tk.Frame(frame, bg=InfoTH.Listview.sep)
-        # list view
-        listview = _ListView(frame, InfoTH.Listview.headers, InfoTH.Listview.rows, InfoTH.Listview.sep, pad)
+        _set_vscrollbar_style(**InfoTH.Listview.scrollbar)
+        self._relaunch_button = _Button(frame, **InfoTH.button, **_get_button_relaunch().to_tk)
+        version = tk.Label(frame, bg=InfoTH.bg, **_get_version(app_name, app_version).to_tk)
+        self._listview = _ListView(frame, **InfoTH.Listview.colors, pad=pad)
+        separator = tk.Frame(frame, bg=TH.separator)
         # layout
-        label.grid(row=0, padx=pad, sticky=tk.W)
-        relaunch_button.grid(row=0, column=1, padx=pad, pady=pad, sticky=tk.EW)
-        relaunch_button.grid_remove()
+        version.grid(row=0, padx=pad, sticky=tk.W)
+        self._relaunch_button.grid(row=0, column=1, padx=pad, pady=pad, sticky=tk.EW)
+        self._relaunch_button.grid_remove()
         frame.columnconfigure(1, weight=1)
-        sep.grid(row=1, columnspan=2, sticky=tk.EW)
-        listview.grid(row=2, columnspan=2, sticky=tk.NSEW)
+        separator.grid(row=1, columnspan=2, sticky=tk.EW)
+        self._listview.grid(row=2, columnspan=2, sticky=tk.NSEW)
         frame.rowconfigure(2, weight=1)
-        # for later use
-        self._relaunch_button = relaunch_button
-        self._listview = listview
-
-    def set(self, infos: list[Info], player_relaunch: Optional[Callable[[], None]]) -> bool:
-        self._listview.set(self._headers, [get_row(info) for info in infos])
-        valid = _are_infos_valid(infos)
-        if not valid and player_relaunch:
-            self._set_relaunch(player_relaunch)
-        self.geometry("")  # enable resizing
-        return valid
 
     def _set_relaunch(self, player_relaunch: Callable[[], None]) -> None:
         def relaunch(_) -> None:
@@ -159,13 +127,21 @@ class _InfosWindow(_StickyWindow):
         self._relaunch_button.bind("<Button-1>", relaunch)
 
     def _bind_mouse_hover(self, widget: tk.Widget) -> None:
-        def show(_):
+        def show(_) -> None:
             """keep showing only when already there to avoid showing again when fading out"""
             if self.attributes("-alpha") == 1.0:
                 self.show()
 
         widget.bind("<Enter>", show, add="+")
         widget.bind("<Leave>", lambda _: self.hide(), add="+")
+
+    def set(self, infos: list[Info], player_relaunch: Optional[Callable[[], None]]) -> bool:
+        self._listview.set(self._headers, [_get_row(info) for info in infos])
+        valid = _are_infos_valid(infos)
+        if not valid and player_relaunch:
+            self._set_relaunch(player_relaunch)
+        self.geometry("")  # enable resizing
+        return valid
 
     def show(self) -> None:
         self._fade.fade(fade_duration_ms=250, out=False)
@@ -185,11 +161,11 @@ class _LogoWindow(_StickyWindow):
         super().__init__(_LogoWindow._offset, takefocus=0)
         self._infos = infos
         self._image = tk.PhotoImage(file=logo_path)  # keep a reference for tkinter
-        label = tk.Label(self, bg=_LogoWindow._pulse_ok.color1, image=self._image, takefocus=0)
-        label.pack()
+        logo = tk.Label(self, bg=_LogoWindow._pulse_ok.color1, image=self._image, takefocus=0)
+        logo.pack()
         self.bind("<Enter>", lambda _: self._infos.show())
         self.bind("<Leave>", lambda _: self._infos.hide())
-        self._pulse = _Pulse(label)
+        self._pulse = _Pulse(logo)
         self.set_pulse(ok=True)
 
     def set_pulse(self, ok: bool) -> None:
@@ -234,10 +210,10 @@ class UI(tk.Tk):
         self.withdraw()  # we rely on some _StickyWindow instead
         self._splash_img = tk.PhotoImage(file=splash_path)  # keep a reference for tk
         self.wm_iconphoto(True, self._splash_img)
-        self._app_name = app_name
         self.splash = _SplashWindow(self._splash_img)
         self._infos = _InfosWindow(app_name, app_version)
         self._logo = _LogoWindow(logo_path, self._infos)
+        self._app_name = app_name
 
     def run_in_thread(
         self, catch_exception: type[Exception], target: Callable[..., None], *args: Any, **kwargs: Any
